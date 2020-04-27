5d91d526851a8811820f9a3e547e18635c75e47b
luiza <luiza_andres@uol.com.br>
2020-04-27 20:30:48 -0300

    fix buttons

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace CsWinForms
         }
     }
     public class TelaInicial : Form {
+        Label lblTitulo;
         Button btnCadastraCliente;
         Button btnListaClientes;
         Button btnDetalhaCliente;
@@ -28,6 +29,16 @@ namespace CsWinForms
         Button btnCadastaLocacao;
         Button btnListaLocacoes;
         public TelaInicial(){
+            this.Text = "BlockBuster";
+            this.BackColor = Color.Beige;
+            this.Size = new Size(300,400);
+
+            lblTitulo = new Label();
+            lblTitulo.Text = "Blockbuster";
+            lblTitulo.Width = 100;
+            lblTitulo.Left = (this.Width - lblTitulo.Width) / 2;
+            this.Controls.Add(lblTitulo);
+            
             btnCadastraCliente = new Button();
             btnCadastraCliente.Size = new Size(200, 20);
             btnCadastraCliente.Location = new Point(40, 30);
@@ -35,22 +46,67 @@ namespace CsWinForms
             this.Controls.Add(btnCadastraCliente);
             btnCadastraCliente.Click += new EventHandler(btnCadastraClienteClick);
 
+            btnListaClientes = new Button();
+            btnListaClientes.Size = new Size(200, 20);
+            btnListaClientes.Location = new Point(40, 60);
+            btnListaClientes.Text = "Listar Clientes";
+            this.Controls.Add(btnListaClientes);
+            //btnListaClientes.Click += new EventHandler(btnListaClientesClick);
+
+            btnDetalhaCliente = new Button();
+            btnDetalhaCliente.Size = new Size(200, 20);
+            btnDetalhaCliente.Location = new Point(40, 90);
+            btnDetalhaCliente.Text = "Detalha clientes";
+            this.Controls.Add(btnDetalhaCliente);
+            //btnDetalhaCliente.Click += new EventHandler(btnDetalhaClienteClick);
+
             btnCadastraFilme = new Button();
             btnCadastraFilme.Size = new Size(200, 20);
-            btnCadastraFilme.Location = new Point(40, 60);
+            btnCadastraFilme.Location = new Point(40, 120);
             btnCadastraFilme.Text = "Cadastra Filme";
             this.Controls.Add(btnCadastraFilme);
             btnCadastraFilme.Click += new EventHandler(CadastraFilmeClick);
+
+            btnListaFilmes = new Button();
+            btnListaFilmes.Size = new Size(200, 20);
+            btnListaFilmes.Location = new Point(40, 150);
+            btnListaFilmes.Text = "Lista de Filmes";
+            this.Controls.Add(btnListaFilmes);
+            //btnListaFilmes.Click += new EventHandler(btnListaFilmesClick);
+
+            btnDetalhaFilme = new Button();
+            btnDetalhaFilme.Size = new Size(200, 20);
+            btnDetalhaFilme.Location = new Point(40, 180);
+            btnDetalhaFilme.Text = "Detalha Filme";
+            this.Controls.Add(btnDetalhaFilme);
+            //btnDetalhaFilme.Click += new EventHandler(btnDetalhaFilmeClick);
+
+            btnCadastaLocacao = new Button();
+            btnCadastaLocacao.Size = new Size(200, 20);
+            btnCadastaLocacao.Location = new Point(40, 210);
+            btnCadastaLocacao.Text = "Cadastar Locação";
+            this.Controls.Add(btnCadastaLocacao);
+            //btnCadastaLocacao.Click += new EventHandler(btnCadastaLocacaoClick);
+
+            btnListaLocacoes = new Button();
+            btnListaLocacoes.Size = new Size(200, 20);
+            btnListaLocacoes.Location = new Point(40, 240);
+            btnListaLocacoes.Text = "Todas as locações";
+            this.Controls.Add(btnListaLocacoes);
+            //btnListaLocacoes.Click += new EventHandler(btnListaLocacoesClick);
         }
         private void btnCadastraClienteClick(object sender, EventArgs e)
         {
+            this.Hide();
             CadastraCliente cadastraCliente = new CadastraCliente();
-            cadastraCliente.ShowDialog();
+            cadastraCliente.Show();
+            
         }
         private void CadastraFilmeClick(object sender, EventArgs e)
         {
+            this.Hide();
             CadastraFilme ListaClientesClick = new CadastraFilme();
-            ListaClientesClick.ShowDialog();
+            ListaClientesClick.Show();
         }
     }
     public class CadastraCliente : Form {
@@ -64,10 +120,9 @@ namespace CsWinForms
         TextBox txtDiasDev;
         Button btnConfirma;
         Button btnCancela;
-         
         public CadastraCliente(){
             int x = 20;
-            this.Text = "BlockBuster";
+            this.Text = "Cadastra Cliente";
             this.BackColor = Color.Beige;
 
             lblNome = new Label();
@@ -77,7 +132,6 @@ namespace CsWinForms
             txtNome = new TextBox();
             txtNome.Location = new Point(x,45);
             txtNome.Size = new Size(180,20);
-
             
             lblDtNasc = new Label();
             lblDtNasc.Text = "Dt. Nasc:";
@@ -86,7 +140,6 @@ namespace CsWinForms
             txtDtNasc = new TextBox();
             txtDtNasc.Location = new Point(x,105);
             txtDtNasc.Size = new Size(180,20);
-
             
             lblCpf = new Label();
             lblCpf.Text = "CPF: ";
@@ -128,35 +181,29 @@ namespace CsWinForms
             this.Controls.Add(btnCancela);
             btnCancela.Click += new EventHandler(btnCancelaClick);
         }
-    
         private void btnConfirmaClick(object sender, EventArgs e)
         {
             MessageBox.Show("Confirmado!!");
             this.Close();
-
+            TelaInicial telaInicial = new TelaInicial();
+            telaInicial.Show();
         }
-
         private void btnCancelaClick(object sender, EventArgs e)
         {
             MessageBox.Show("Cancelado!!");
             this.Close();
+            TelaInicial telaInicial = new TelaInicial();
+            telaInicial.Show();
         }
     }
     public class CadastraFilme : Form {
         Label lblNome;
-        Label lblDtNasc;
-        Label lblCpf;
-        Label lblDiasDev;
         TextBox txtNome;
-        TextBox txtDtNasc;
-        TextBox txtCpf;
-        TextBox txtDiasDev;
         Button btnConfirma;
         Button btnCancela;
-         
         public CadastraFilme(){
             int x = 20;
-            this.Text = "BlockBuster";
+            this.Text = "Cadastra Filme";
             this.BackColor = Color.Beige;
 
             lblNome = new Label();
@@ -185,19 +232,20 @@ namespace CsWinForms
             btnCancela.Text = "Cancela";
             this.Controls.Add(btnCancela);
             btnCancela.Click += new EventHandler(btnCancelaClick);
-
         }
-    
         private void btnConfirmaClick(object sender, EventArgs e)
         {
             MessageBox.Show("Confirmado!!");
             this.Close();
+            TelaInicial telaInicial = new TelaInicial();
+            telaInicial.Show();
         }
-
         private void btnCancelaClick(object sender, EventArgs e)
         {
             MessageBox.Show("Cancelado!!");
             this.Close();
+            TelaInicial telaInicial = new TelaInicial();
+            telaInicial.Show();
         }
     }
 }
